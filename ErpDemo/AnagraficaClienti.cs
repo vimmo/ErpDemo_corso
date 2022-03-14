3a086a73c0f75f4fa9b79093a0836d405952967a
Vincenzo Immordino <vimmordino@columbusinformatica.it>
2022-03-14 22:11:51 +0100

    pulizia funzioni

--- ErpDemo/AnagraficaClienti.cs
+++ ErpDemo/AnagraficaClienti.cs
@@ -19,25 +19,18 @@ namespace ErpDemo
         {
             InitializeComponent();
             txtId.Enabled = false;
-            ChangeFieldsState();
+            CambiaStatoCampi(DOCUMENT_MODE != _DOC_MODE.BROWSE);
 
             _db = new DBClientiService();
         }
-        private void ChangeFieldsState()
+        private void CambiaStatoCampi(bool setEnabled)
         {
-            txtRagioneSociale.Enabled = (DOCUMENT_MODE != _DOC_MODE.BROWSE);
-            txtIndirizzo.Enabled = (DOCUMENT_MODE != _DOC_MODE.BROWSE);
-            txtCitta.Enabled = (DOCUMENT_MODE != _DOC_MODE.BROWSE);
-            txtSettore.Enabled = (DOCUMENT_MODE != _DOC_MODE.BROWSE);
+            txtRagioneSociale.Enabled = setEnabled;
+            txtIndirizzo.Enabled = setEnabled;
+            txtCitta.Enabled = setEnabled;
+            txtSettore.Enabled = setEnabled;
         }
-        private void ChangeFieldsState(bool setReadOnly)
-        {
-            txtRagioneSociale.Enabled = setReadOnly;
-            txtIndirizzo.Enabled = setReadOnly;
-            txtCitta.Enabled = setReadOnly;
-            txtSettore.Enabled = setReadOnly;
-        }
-        private void ClearData()
+        private void PulisciCampi()
         {
             txtId.Clear();
             txtRagioneSociale.Clear();
@@ -46,45 +39,55 @@ namespace ErpDemo
             txtSettore.Clear();
             lblRagSoc.ForeColor = Color.Black;
         }
+        private void RiempiCampi(Clienti cliente)
+        {
+            txtId.Text = cliente.Id.ToString();
+            txtRagioneSociale.Text = cliente.RagioneSociale;
+            txtIndirizzo.Text = cliente.Indirizzo;
+            txtCitta.Text = cliente.Citta;
+            txtSettore.Text = cliente.Settore;
+        }
         public override void OnNew()
         {
-            ChangeFieldsState();
-            ClearData();
+            CambiaStatoCampi(DOCUMENT_MODE != _DOC_MODE.BROWSE);
+            PulisciCampi();
         }
         public override void OnEdit()
         {
-            ChangeFieldsState();
+            CambiaStatoCampi(DOCUMENT_MODE != _DOC_MODE.BROWSE);
         }
         public override bool OnDelete()
         {
-            bool bOk = true;
+
             if (MessageBox.Show("Confermi eliminazione?",
                 "Elimina",
                 MessageBoxButtons.OKCancel,
-                MessageBoxIcon.Exclamation) == DialogResult.OK)
-                bOk = true;
-            else
-                bOk = false;
-
-            if(bOk)
+                MessageBoxIcon.Exclamation) == DialogResult.Cancel)
+                return false;
+  
+            var cliente = new Clienti
             {
-                // fai qualcosa
-                var cliente = new Clienti
-                {
-                    Id = Convert.ToInt32(txtId.Text),
-                    RagioneSociale = txtRagioneSociale.Text,
-                    Indirizzo = txtIndirizzo.Text,
-                    Citta = txtCitta.Text,
-                    Settore = txtSettore.Text
-                };
-                bOk = _db.EliminaCliente(cliente);
+                Id = Convert.ToInt32(txtId.Text),
+                RagioneSociale = txtRagioneSociale.Text,
+                Indirizzo = txtIndirizzo.Text,
+                Citta = txtCitta.Text,
+                Settore = txtSettore.Text
+            };
+            bool bOk = _db.EliminaCliente(cliente);
+            
+            if(!bOk)
+            {
+                MessageBox.Show(
+                      "Errore in salvataggio",
+                      "Errore",
+                      MessageBoxButtons.OK,
+                      MessageBoxIcon.Error);
             }
 
             return bOk;
         }
         public override bool OnSave()
         {
-            bool bOk = true;
 
             if (txtRagioneSociale.Text == "")
             {
@@ -98,34 +101,33 @@ namespace ErpDemo
                 "Salva",
                 MessageBoxButtons.OKCancel,
                 MessageBoxIcon.Question) == DialogResult.Cancel)
-                bOk = false;
+                return false;
 
-            if (bOk)
+
+            var cliente = new Clienti
             {
-                var cliente = new Clienti
-                {
-                    Id = DOCUMENT_MODE == _DOC_MODE.NEW ? 0 : Convert.ToInt32(txtId.Text),
-                    RagioneSociale = txtRagioneSociale.Text,
-                    Indirizzo = txtIndirizzo.Text,
-                    Citta = txtCitta.Text,
-                    Settore = txtSettore.Text
-                };
-                if (DOCUMENT_MODE == _DOC_MODE.NEW)
-                {
-                    Clienti cli = _db.CreaCliente(cliente);
-                    txtId.Text = cli.Id.ToString();
-                    bOk = txtId.Text != "";
-                }
-                else
-                {
-                    bOk = _db.ModificaCliente(cliente);
-                }
-                
-            }
+                Id = DOCUMENT_MODE == _DOC_MODE.NEW ? 0 : Convert.ToInt32(txtId.Text),
+                RagioneSociale = txtRagioneSociale.Text,
+                Indirizzo = txtIndirizzo.Text,
+                Citta = txtCitta.Text,
+                Settore = txtSettore.Text
+            };
 
+            bool bOk;
+            if (DOCUMENT_MODE == _DOC_MODE.NEW)
+            {
+                Clienti cli = _db.CreaCliente(cliente);
+                txtId.Text = cli.Id.ToString();
+                bOk = txtId.Text != "";
+            }
+            else
+            {
+                bOk = _db.ModificaCliente(cliente);
+            }
+               
 
             if (bOk)
-                ChangeFieldsState(false);
+                CambiaStatoCampi(false);
             else
                 MessageBox.Show(
                     "Errore in " + (DOCUMENT_MODE == _DOC_MODE.NEW ? "inserimento" : "modifica"),
@@ -137,20 +139,17 @@ namespace ErpDemo
         }
         public override bool OnCancel()
         {
-            bool bOk = true;
 
             if (MessageBox.Show("Annullare modifiche?",
                 "Annulla",
                 MessageBoxButtons.YesNo,
                 MessageBoxIcon.Question) == DialogResult.No)
-                bOk = false;
+                return false;
 
-            if (bOk)
-            {
-                ChangeFieldsState(false);
-                ClearData();
-            }
-            return bOk;
+            CambiaStatoCampi(false);
+            PulisciCampi();
+            
+            return true;
         }
 
         public override void OnFirst()
@@ -162,14 +161,7 @@ namespace ErpDemo
             RiempiCampi(_db.LeggiCliente(true));
         }
 
-        private void RiempiCampi(Clienti cliente)
-        {
-            txtId.Text = cliente.Id.ToString();
-            txtRagioneSociale.Text = cliente.RagioneSociale;
-            txtIndirizzo.Text = cliente.Indirizzo;
-            txtCitta.Text = cliente.Citta;
-            txtSettore.Text = cliente.Settore;
-        }
+
 
 
 
